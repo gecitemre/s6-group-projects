75a6558ee5c1fe8f0ecc9b869605d678d4de163c
emregecit2 <emregecit2@gmail.com>
2023-05-24 10:18:34 +0300

    add the3.c

---
+++ ceng336/THE3/THE3.X/the3.c
@@ -0,0 +1,15 @@
+/*
+ * File:   the3.c
+ * Author: e2521581
+ *
+ * Created on May 24, 2023, 10:17 AM
+ */
+
+
+#include <xc.h>
+#include "lcd.h"
+#include "the3.h"
+
+void main(void) {
+    return;
+}
