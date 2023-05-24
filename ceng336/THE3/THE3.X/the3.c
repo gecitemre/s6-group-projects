47b06f26b3f279c1b0f7e882190b410332032c3f
y4nci <baran.yanci@metu.edu.tr>
2023-05-24 15:10:57 +0300

    small additions

--- ceng336/THE3/THE3.X/the3.c
+++ ceng336/THE3/THE3.X/the3.c
@@ -10,6 +10,30 @@
 #include "lcd.h"
 #include "the3.h"
 
+/**
+ * RB0 = throw
+ *      - operate iff there is a player on the same cell that frisbee locates
+ *        and the game is in INACTIVE_MODE
+ *      - make the game mode ACTIVE_MODE
+ *      - INT0
+ * RB1 = switch player
+ *      - no switch if inactive & the selected player holds the frisbee
+ *      - order does not matter
+ *      - INT1
+ * RA3,RA4,RA5 = DISP2,DISP3,DISP4
+ *      - DISP3 = '-'
+ */
+
+unsigned int teamA_score = 0;
+unsigned int teamB_score = 0;
+
+void initPorts() {
+    TRISB = 255; // 11111111
+}
+
+void initInterrupts() {
+}
+
 void main(void) {
     return;
 }
