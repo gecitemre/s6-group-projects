76403385480a80f9b903506422b3a954d7522783
y4nci <baran.yanci@metu.edu.tr>
2023-05-27 22:31:04 +0300

    resolve #27

--- ceng336/THE3/THE3.X/the3.c
+++ ceng336/THE3/THE3.X/the3.c
@@ -47,7 +47,46 @@ void TMR0Interrupt()
     remaining_frisbee_moves--;
     DisplayObject(&objects[FRISBEE_INDEX]);
 
+    // if there are no remaining moves, we will check who has the frisbee.
+    // we will set the game mode to INACTIVE_MODE.
+    // if team A has the frisbee, we will increment team A's score.
+    // if team B has the frisbee, we will increment team B's score.
+    // if no one has the frisbee, we will do nothing.
+    if (!remaining_frisbee_moves)
+    {
+        unsigned int frisbeeCaught = 0;
 
+        ClearObject(&frisbee_target_object);
+        
+        for (unsigned i = 0; i < 4; i++)
+        {
+            if (Collision(&objects[FRISBEE_INDEX], &objects[i]))
+            {
+                frisbeeCaught = 1;
+                
+                if (objects[i].data.type == TEAM_A_PLAYER)
+                {
+                    teamA_score++;
+                }
+                else if (objects[i].data.type == TEAM_B_PLAYER)
+                {
+                    teamB_score++;
+                }
+                
+                ClearObject(&objects[FRISBEE_INDEX]);
+                ClearObject(&objects[i]);
+                objects[i].data.frisbee = 1;
+                DisplayObject(&objects[i]);
+                break;
+            }
+        }
+        
+        if (!frisbeeCaught) DisplayObject(&objects[FRISBEE_INDEX]);
+
+        // we will make the game mode inactive when the frisbee is caught by a player
+        // mode = INACTIVE_MODE;
+    }
+    
     for (unsigned i = 0; i < 4; i++)
     {
         if (objects[i].data.selected)
@@ -79,45 +118,6 @@ try_different_movement:
         }
         
         MovePlayer(i, horizontal, vertical);
-    }
-
-    // if there are no remaining moves, we will check who has the frisbee.
-    // we will set the game mode to INACTIVE_MODE.
-    // if team A has the frisbee, we will increment team A's score.
-    // if team B has the frisbee, we will increment team B's score.
-    // if no one has the frisbee, we will do nothing.
-    if (!remaining_frisbee_moves)
-    {
-        unsigned int frisbeeCaught = 0;
-        
-        for (unsigned i = 0; i < 4; i++)
-        {
-            if (Collision(&objects[FRISBEE_INDEX], &objects[i]))
-            {
-                frisbeeCaught = 1;
-                
-                if (objects[i].data.type == TEAM_A_PLAYER)
-                {
-                    teamA_score++;
-                }
-                else if (objects[i].data.type == TEAM_B_PLAYER)
-                {
-                    teamB_score++;
-                }
-                ClearObject(&objects[i]);
-                objects[i].data.frisbee = 0;
-                DisplayObject(&objects[i]);
-                break;
-            }
-        }
-
-        ClearObject(&objects[FRISBEE_INDEX]);
-        ClearObject(&frisbee_target_object);
-        
-        if (!frisbeeCaught) DisplayObject(&objects[FRISBEE_INDEX]);
-
-        // we will make the game mode inactive when the frisbee is caught by a player
-        // mode = INACTIVE_MODE;
     }
 }
 
