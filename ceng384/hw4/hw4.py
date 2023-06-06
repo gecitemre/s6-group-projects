2b9a619575f54c838ff43ca680577da06120e009
emregecit2 <emregecit2@gmail.com>
2023-06-07 00:36:54 +0300

    implement fft

--- ceng384/hw4/hw4.py
+++ ceng384/hw4/hw4.py
@@ -1,6 +1,21 @@
+import numpy
 from scipy.io import wavfile
 
-def fft():
-    pass
+def fft(x):
+    N = len(x)
+    if N <= 1:
+        return x
+    odd = fft(x[::2]) # check indexing NOTE
+    even = fft(x[1::2]) # check indexing NOTE
 
-messsage = wavfile.read('encoded.wav')
+    return numpy.concatenate([odd + even * numpy.exp(-2j * numpy.pi * numpy.arange(N // 2) / N),
+                              odd - even * numpy.exp(-2j * numpy.pi * numpy.arange(N // 2) / N)])
+
+rate, message = wavfile.read("encoded.wav")
+message = fft(message)
+first_half = message[:len(message) // 2 + 1]
+second_half = message[len(message) // 2 + 1:]
+message2 = numpy.concatenate([numpy.flip(first_half), numpy.flip(second_half)])
+message3 = numpy.fft.ifft(message2)
+
+wavfile.write("decoded.wav", rate, message3.astype(numpy.int16))
