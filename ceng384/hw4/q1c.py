78d42246363a63b5e75617cd5f18fe1bd2277913
gecitemre <emregecit5@gmail.com>
2023-06-08 23:53:02 +0300

    change q1c

--- ceng384/hw4/q1c.py
+++ ceng384/hw4/q1c.py
@@ -2,6 +2,6 @@ import numpy
 from matplotlib import pyplot
 
 t = numpy.linspace(-100, 100, 1000)
-y = 3 * numpy.exp(-2 * t) - 3 * numpy.exp(-t) * numpy.heaviside(t, 1)
+y = 3 * numpy.exp(-2 * t)* numpy.heaviside(t, 1) - 3 * numpy.exp(-t) 
 pyplot.plot(t, y)
 pyplot.savefig("figures/q1c.pgf")
