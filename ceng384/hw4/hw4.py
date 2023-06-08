8904c96a3499ea6a34cc112e062daf3ec9e2ebde
gecitemre <emregecit5@gmail.com>
2023-06-08 17:31:12 +0300

    q5 bug fix, and pgf support

--- ceng384/hw4/hw4.py
+++ ceng384/hw4/hw4.py
@@ -1,26 +1,38 @@
 import numpy
 from scipy.io import wavfile
 from tester import test # This is to verify the implementation, uses numpy.fft
+import matplotlib
+from matplotlib import pyplot
+
+matplotlib.use("pgf")
 
 def fft(x: numpy.ndarray) -> numpy.ndarray:
     N = len(x)
     if N <= 1:
         return x
 
-    # Even and odd with mathematical indexing
-    odd = fft(x[::2])
-    even = fft(x[1::2]) * numpy.exp(-2j * numpy.pi * numpy.arange(N // 2) / N)
-    return numpy.concatenate([odd + even,
-                              odd - even])
+    even = fft(x[::2])
+    odd = fft(x[1::2]) * numpy.exp(-2j * numpy.pi * numpy.arange(N // 2) / N)
+    return numpy.concatenate([even + odd,
+                              even - odd])
 
 def ifft(X: numpy.ndarray) -> numpy.ndarray:
-    return fft(X)[::-1] / len(X)
+    return numpy.flip(fft(X)) / len(X)
 
+def plot(signal, name):
+    pyplot.plot(signal)
+    pyplot.savefig("figures/" + name + ".pgf")
+    pyplot.clf()
 
 rate, encoded = wavfile.read("encoded.wav")
+plot(encoded, "encoded")
 N = len(encoded)
-dft_coefficients = fft(encoded)
-flipped = numpy.flip(dft_coefficients)
-decoded = ifft(numpy.concatenate([flipped[N//2 + 1:], flipped[:N//2 + 1]]))
-test(encoded, decoded)
+dft_encoded = fft(encoded)
+plot(dft_encoded.real, "dft_encoded")
+flipped = numpy.flip(dft_encoded)
+dft_decoded = numpy.concatenate([flipped[N//2:], flipped[:N//2]])
+plot(dft_decoded.real, "dft_decoded")
+decoded = ifft(dft_decoded)
+plot(decoded.real, "decoded")
+assert test(encoded, decoded)
 wavfile.write("decoded.wav", rate, decoded.real.astype(numpy.int16))
--- ceng384/hw4/tester.py
+++ ceng384/hw4/tester.py
@@ -1,12 +1,12 @@
 from numpy.fft import fft, ifft
-from numpy import flip, concatenate, int16
+from numpy import flip, concatenate
 from scipy.io import wavfile
 
 def test(encoded, decoded):
     N = len(encoded)
     reference = fft(encoded)
     flipped_reference = flip(reference)
-    decoded_reference = ifft(concatenate([flipped_reference[N//2 + 1:], flipped_reference[:N//2 + 1]]))[1:]
+    decoded_reference = ifft(concatenate([flipped_reference[N//2:], flipped_reference[:N//2]]))[1:]
     diff = decoded_reference - decoded[:-1]
     condition = diff < 1e-7
-    print("Test passed:", condition.all())
+    return condition.all()
