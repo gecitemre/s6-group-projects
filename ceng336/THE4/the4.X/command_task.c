4a31a721c85d28b877a4639fd7187b5ee28d0c50
gecitemre <emregecit5@gmail.com>
2023-06-22 22:25:58 +0300

    reset output buffer

--- ceng336/THE4/the4.X/command_task.c
+++ ceng336/THE4/the4.X/command_task.c
@@ -13,6 +13,9 @@ TASK(COMMAND_TASK)
         ClearEvent(ALARM_EVENT_MASK);
         TXREG = *output_pointer;
         if (*output_pointer == ':') {
+            // reset output buffer to $W:
+            output_buffer[1] = 'W';
+            output_buffer[2] = ':';
             output_pointer = output_buffer;
             WaitEvent(COMMAND_EVENT_MASK);
             ClearEvent(COMMAND_EVENT_MASK);
